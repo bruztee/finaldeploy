3adb5b8be4a6d3dd04dd3c79cdaf885da77a43ec
bruztee <diary3405@gmail.com>
2025-03-01 23:47:55 +0200

    Update server.js

--- deploy2/src/server/server.js
+++ deploy2/src/server/server.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === 'development') {
   app.use(express.static('dist'));
 }
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 const server = app.listen(port);
 console.log(`Server listening on port ${port}`);
 
